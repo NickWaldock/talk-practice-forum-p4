ee917d6aa08206292d59562826ccd075ac0c64bb
NickWaldock <nickwaldock@me.com>
2023-03-16 13:57:15 +0000

    Code Post model

--- talk_practice_forum_p4/blog/models.py
+++ talk_practice_forum_p4/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(model.Models):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    subtitle = models.CharField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    created_on = models.DateField(auto_now=True)
+    updated_on = models.DateField(auto_now=True)
+    body = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt  = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+        
+
+    class Meta:
+        ordering = ['-created_on']
+
+
+    def __str__(self):
+        return. self.title
+
+
+    def number_of_likes(self):
+        return self.likes.count()
