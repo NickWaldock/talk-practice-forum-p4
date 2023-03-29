549625c7ae5c1588a7807d42d52e917f849a7ea6
NickWaldock <nickwaldock@me.com>
2023-03-29 13:28:24 +0000

    Tidy up admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -4,15 +4,11 @@ from django_summernote.admin import SummernoteModelAdmin
 
 admin.site.register(Category)
 
-
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'created_on', 'category')
-    # list_display = ('title', 'author', 'slug', 'status', 'created_on')
     search_fields = ['title', 'subtitle', 'content']
-    # prepopulated_fields = {'slug': ('title',)}
     list_filter = ('created_on', 'author')
-    # list_filter = ('status', 'created_on')
     summernote_fields = ('body')
 
 
