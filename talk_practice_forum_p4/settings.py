fc381dbe58ce53067df6c1728fae9527455b8efd
NickWaldock <nickwaldock@me.com>
2023-03-30 12:34:10 +0000

    Add runtime.txt for heroku deployment

--- talk_practice_forum_p4/settings.py
+++ talk_practice_forum_p4/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'talk_practice_forum_p4.wsgi.application'
 # }
 
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
