8d17d6d0df515583eecc5ef3eb46eaacb644ceb2
NickWaldock <nickwaldock@me.com>
2023-03-30 13:57:46 +0000

    Change urls for landing page to be visible first

--- blog/urls.py
+++ blog/urls.py
@@ -3,15 +3,19 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('welcome/', views.Welcome.as_view(), name='welcome'),
+    path('', views.Welcome.as_view(), name='welcome'),
+    path('home/', views.PostList.as_view(), name='home'),
     path('add-post/', views.AddPost.as_view(), name='add-post'),
     path('article/<int:pk>', views.PostView.as_view(), name='article'),
-    path('article/update/<int:pk>', views.UpdatePost.as_view(), name='update-post'),
-    path('article/delete-post/<int:pk>', views.DeletePost.as_view(), name='delete-post'),
+    path('article/update/<int:pk>',
+         views.UpdatePost.as_view(),
+         name='update-post'),
+    path('article/delete-post/<int:pk>',
+         views.DeletePost.as_view(),
+         name='delete-post'),
     path('like/<int:pk>', views.LikeView, name='like-post'),
     path('add-category/', views.AddCategory.as_view(), name='add-category'),
     path('category/<str:category>/', views.Categories, name='category'),
-    path('category-list/', views.CategoryList, name='category-list'), 
+    path('category-list/', views.CategoryList, name='category-list'),
     path('contact/', views.ContactView.as_view(), name='contact'),
 ]
